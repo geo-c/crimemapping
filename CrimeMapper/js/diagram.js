8523b87f923728e43f58046ec6b7716a58345e4a
Antonia <antoniaicq@web.de>
2016-12-13 12:52:07 +0100

    Added example bar chart (random values, only for one year and borough)

--- CrimeMapper/js/diagram.js
+++ CrimeMapper/js/diagram.js
@@ -1,24 +1,46 @@
-/*Canavas JS*/
 window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        theme: "theme2",//theme1
-        title:{
-            text: "Basic Column Chart - CanvasJS"
+
+    console.log("create diagram");
+
+    var chart = c3.generate({
+        bindto: '#barDiagram',
+        data: {
+            x: 'x',
+            //xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+            columns: [
+                ['x', '2013-01-01', '2013-02-02', '2013-03-03', '2013-04-04', '2013-05-05', '2013-06-06', '2013-07-01', '2013-08-01', '2013-09-01', '2013-10-01', '2013-11-01', '2013-12-01'],
+              //  ['x', '201301', '201302', '201303', '201304', '201305', '201306'],
+                ['BicycleTheft', 30, 200, 200, 400, 150, 250, 30, 200, 200, 400, 150, 250],
+                ['Burglary', 130, 100, 100, 200, 150, 50, 30, 200, 200, 400, 150, 250],
+                ['Other theft', 230, 200, 200, 300, 250, 250, 30, 200, 200, 400, 150, 250]
+            ],
+            type: 'bar',
+            groups: [
+                ['BicycleTheft', 'Burglary', 'Other theft']
+            ]
         },
-        animationEnabled: false,   // change to true
-        data: [
-            {
-                // Change type to "bar", "area", "spline", "pie",etc.
-                type: "column",
-                dataPoints: [
-                    { label: "apple",  y: 10  },
-                    { label: "orange", y: 15  },
-                    { label: "banana", y: 25  },
-                    { label: "mango",  y: 30  },
-                    { label: "grape",  y: 28  }
-                ]
+        grid: {
+            y: {
+                lines: [{value:0}]
             }
-        ]
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m'
+                }
+            },
+            y: {
+                label: { // ADD
+                    text: 'Number of crimes',
+                    position: 'outer-middle'
+                }
+            }
+        },
+        size: {
+            height: 480
+        }
     });
-    chart.render();
-}
+
+};
