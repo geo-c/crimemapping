a2557318e3c93a75297212b9a45daf0231ea1be2
Wittkopp <j_witt10@uni-muenster.de>
2016-12-14 19:23:50 +0100

    Heatmap#1
    New getJSON for OCT-Data. Finer tuning of properties and weighting of
    crimes possible

--- CrimeMapper/js/map.js
+++ CrimeMapper/js/map.js
@@ -5,30 +5,30 @@ function coordinate(x, y) {
     this.x = parseFloat(x);
     this.y = y;
 }
-
-var jqxhr = $.getJSON( "sparql", function() {
+var url= "http://giv-oct.uni-muenster.de:8080/api/dataset/crimeLoc?authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbmFtZSI6IkNyaW1lIE1hcHBlciIsImlhdCI6MTQ4MDY3ODM5Nn0.G8E9xAK7OoXhjpmc2RPd4ZMzqbA-6P38rONMc4H6_Ng"
+var jqxhr = $.getJSON( url, function() {
   	console.log( "success" );
 	}).done(function() {
 		console.log("second success");
-		var JSONtext = jqxhr.responseText;
-		var CrimeLocation= JSON.parse(JSONtext);
-		for (var key in CrimeLocation.results.bindings){
-		CrimeLatLon.push(new coordinate(CrimeLocation.results.bindings[key].lat.value,CrimeLocation.results.bindings[key].lon.value));
-		/*CrimeLon.push(CrimeLocation.results.bindings[key].lon.value);*/
+		var JSONtext = jqxhr.responseJSON;
+		/*var CrimeLocation= JSON.parse(JSONtext);*/
+		console.log(JSONtext)
+		
+		for (var key in JSONtext){
+		CrimeLatLon.push(new coordinate(JSONtext[key].lon.value,JSONtext[key].lat.value));
 		}
-		/*  for (var i = 0; i < CrimeLatLon.length; i++) {
-	   marker = new L.marker([CrimeLatLon[i].x,CrimeLatLon[i].y])
-		.addTo(map);
-	  }*/
-
+		/*for (var key in CrimeLocation.results.bindings){
+		CrimeLatLon.push(new coordinate(CrimeLocation.results.bindings[key].lat.value,CrimeLocation.results.bindings[key].lon.value));
+		}*/
+		
 		for (var i = 1; i < CrimeLatLon.length; i++) {
-			CrimeHeat.push([CrimeLatLon[i].x, CrimeLatLon[i].y, 1])
+			CrimeHeat.push([CrimeLatLon[i].x, CrimeLatLon[i].y,1])
 		}
 		console.log(CrimeHeat);
-
-		var heat = L.heatLayer(CrimeHeat, {radius: 35})
+		var heat = L.heatLayer(CrimeHeat, {radius: 20})
 			.addTo(map);
-
+			
+			
 	}).fail(function() {
 			console.log( "error" );
 	}).always(function() {
